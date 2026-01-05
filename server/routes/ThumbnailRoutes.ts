40866fbb93f4fe4287f5529e571c7bea77ef41b8
Rusiru49 <rusiruxz@gmail.com>
2026-01-05 15:31:07 +0530

    add new routes

--- server/routes/ThumbnailRoutes.ts
+++ server/routes/ThumbnailRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { deleteThumbnail, generateThumbnail } from '../controllers/ThumbnailController.js';
+import protect from '../middlewares/auth.js';
 
 const ThumbnailRoutes = express.Router();
 
-ThumbnailRoutes.post('/generate', generateThumbnail)
-ThumbnailRoutes.delete('/delete/:id', deleteThumbnail)
+ThumbnailRoutes.post('/generate', protect, generateThumbnail)
+ThumbnailRoutes.delete('/delete/:id', protect, deleteThumbnail)
 
 export default ThumbnailRoutes;
